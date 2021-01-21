f3ae2056299f6d34646a97f729d15d4c0a1aaf73
Yang Yang <yyang0729@gmail.com>
2021-01-20 22:47:59 -0800

    enhanced modular arithmetic analysis program.

--- modular_arithmetic.py
+++ modular_arithmetic.py
@@ -1,17 +1,20 @@
-
+import csv
 import math
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sympy import *
+import seaborn as sns
 
 
-def gen_solinas_matrix(i, j):
+def gen_solinas_matrix(i, j, sign=1):
     gcd = math.gcd(i, j)
     d = i // gcd
 
     #print('degree:', d)
 
     c = (d + 1) * [0]
-    c[d] = -1
+    c[d] = -1 * sign
     #c[j // gcd + 1] = 1
     c[d - j // gcd] = 1
 
@@ -33,8 +36,9 @@ def gen_solinas_matrix(i, j):
             else:
                 m[i][j] = m[i-1][j-1] + m[i-1][d-1] * c[d-j]
 
-    #print('solinas matrix:')
+    print('solinas matrix:')
     print(np.matrix(m))
+    print(gcd)
 
     num_pos = d * [0]
     num_neg = d * [0]
@@ -46,18 +50,50 @@ def gen_solinas_matrix(i, j):
             elif m[i][j] < 0:
                 num_neg[j] += 1
 
-    #print('pos array: ', num_pos, 'max mod add:', max(num_pos))
-    #print('neg array: ', num_neg, 'max mod sub:', max(num_neg))
+    print('pos array: ', num_pos, 'max mod add:', max(num_pos))
+    print('neg array: ', num_neg, 'max mod sub:', max(num_neg))
 
     return max(num_pos) + max(num_neg)
 
-gen_solinas_matrix(64, 24)
+gen_solinas_matrix(60, 21, 1)
 
 
-#for i in range(1, 128):
-#    for j in range(1, i):
-#        num = 2**i - 2**j + 1
-#
-#        if isprime(num):
-#            print(num, ', i:', i, ', j:', j, ', bit length:',
-#                  num.bit_length(), 'total weights:', gen_solinas_matrix(i, j))
+def plot():
+
+    sns.set_theme(style='darkgrid')
+    cmap = sns.color_palette('ch:2,r=.2,l=.6')
+
+    with open('prime.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        csv_writer.writerow(
+            ['prime', 'i', 'j', 'sign', 'bit width', 'mod add/sub'])
+        for i in range(12, 65):
+            for j in range(1, i):
+                for sign in [-1, 1]:
+                    num = 2**i - 2**j + sign * 1
+                    if isprime(num):
+                        print(num, i, j, sign, 'bit length:',
+                              num.bit_length(), 'total weights:', gen_solinas_matrix(i, j, sign))
+                        csv_writer.writerow(
+                            [num, i, j, sign, num.bit_length(), gen_solinas_matrix(i, j, sign)])
+
+    data_df = pd.read_csv('prime.csv')
+
+    df1 = data_df.groupby(['bit width', 'mod add/sub'],
+                          as_index=False)['prime'].count()
+
+    fig, ax = plt.subplots(figsize=(6, 4))
+    ax = sns.scatterplot(x='bit width', y='mod add/sub', hue='prime', size='prime',
+                           sizes=(40, 300), data=df1, palette=cmap)
+    plt.ylim(0, 10.5)
+    ax.set_xlabel('bit width', fontsize=16)
+    ax.set_ylabel('mod add/sub', fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+
+    ax.legend(title='num of\nprimes', bbox_to_anchor=(1.02, 0.8), facecolor='white', edgecolor='white')
+
+    fig.tight_layout()
+    fig.savefig('output.eps')
+
+
+#plot()
