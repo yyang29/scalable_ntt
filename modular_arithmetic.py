aac7db496b7e6ebef261c9175ef7923130b3734e
Yang Yang <yyang0729@gmail.com>
2021-01-22 11:29:50 -0800

    Update solinas logic.

--- modular_arithmetic.py
+++ modular_arithmetic.py
@@ -8,6 +8,9 @@ import seaborn as sns
 
 
 def gen_solinas_matrix(i, j, sign=1):
+    if j == 1 and sign == -1:
+        return 2
+
     gcd = math.gcd(i, j)
     d = i // gcd
 
@@ -46,22 +49,24 @@ def gen_solinas_matrix(i, j, sign=1):
     for j in range(0, d):
         for i in range(0, d):
             if m[i][j] > 0:
-                num_pos[j] += 1
+                num_pos[j] += m[i][j]
             elif m[i][j] < 0:
-                num_neg[j] += 1
+                num_neg[j] -= m[i][j]
 
     print('pos array: ', num_pos, 'max mod add:', max(num_pos))
     print('neg array: ', num_neg, 'max mod sub:', max(num_neg))
 
-    return max(num_pos) + max(num_neg)
+    return max(num_pos) + max(num_neg) + 1
 
-gen_solinas_matrix(60, 21, 1)
+
+gen_solinas_matrix(13, 1, 1)
 
 
 def plot():
 
     sns.set_theme(style='darkgrid')
-    cmap = sns.color_palette('ch:2,r=.2,l=.6')
+
+    visited = set()
 
     with open('prime.csv', 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=',')
@@ -71,7 +76,8 @@ def plot():
             for j in range(1, i):
                 for sign in [-1, 1]:
                     num = 2**i - 2**j + sign * 1
-                    if isprime(num):
+                    if isprime(num) and num not in visited:
+                        visited.add(num)
                         print(num, i, j, sign, 'bit length:',
                               num.bit_length(), 'total weights:', gen_solinas_matrix(i, j, sign))
                         csv_writer.writerow(
@@ -84,16 +90,17 @@ def plot():
 
     fig, ax = plt.subplots(figsize=(6, 4))
     ax = sns.scatterplot(x='bit width', y='mod add/sub', hue='prime', size='prime',
-                           sizes=(40, 300), data=df1, palette=cmap)
+                         sizes=(40, 300), data=df1, palette=sns.color_palette(palette='ch:2,r=.2,l=.6', n_colors=7, as_cmap=True))
     plt.ylim(0, 10.5)
-    ax.set_xlabel('bit width', fontsize=16)
-    ax.set_ylabel('mod add/sub', fontsize=16)
-    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('bit width', fontsize=12)
+    ax.set_ylabel('adder tree inputs', fontsize=12)
+    ax.tick_params(axis='both', which='major', labelsize=12)
 
-    ax.legend(title='num of\nprimes', bbox_to_anchor=(1.02, 0.8), facecolor='white', edgecolor='white')
+    ax.legend(title='num of\nprimes', bbox_to_anchor=(
+        1.0, 0.8), facecolor='white', edgecolor='white', markerscale=0.9)
 
-    fig.tight_layout()
+    fig.tight_layout(pad=0.05)
     fig.savefig('output.eps')
 
 
-#plot()
+plot()
